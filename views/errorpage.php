2ecb72552602952a707f608615030f46301b3a96
Kamau Mbugua <kkibugi@sevenseastech.com>
2016-08-02 10:25:45 +0300

    remove directory views

--- views/errorpage.php
+++
@@ -1,9 +0,0 @@
-<div class="section">
-    <div class="container">
-     <div class="panel panel-default panel-faded">
-                <div class="panel-body">
-    <h1> page not found</h1>
-    </div>
-    </div>
-    </div>
-</div>
